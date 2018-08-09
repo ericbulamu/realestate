c10af588f8214822471803b34417030389bcaa7c
Erik <gestyri@gmail.com>
2018-08-09 16:47:25 +0200

    re
    fgfd

---
+++ readlars 1987-1989.R
@@ -0,0 +1,52 @@
+setwd("C:/Users/User/Desktop/realstate/data/HMS.F1990.LARS/1989")
+
+delimiters <- c(28,8, 4,6,2,3,1, 2,1,4,9,1,4,9,1,4,9,1,4,9,1,4,9,1,1)
+sum(delimiters)
+
+
+dat <- read.fwf("HMD_FACDSB89.txt", delimiters,comment.char="")
+
+
+colnames(dat) <- c("Respondent_Name", 
+                   "Respondent_ID", 
+                   "MSA_of_Report",
+                   "Census_Tract_Number",
+                   "State",
+                   "Country",
+"Supervisory_Agency_COde",
+"Census Validity Flag (02-09, 14,17 are obsolete codes)",
+" VA, FHA, FmHA Loans: Validity Flag", 
+"Va, FHA , FmHa Loans: Number of Loans", 
+"VA, FHA, FmHA  Loans: Total Amount ($)", 
+"Conventional Loans: Validity Flag",
+"Conventional_Loans: Number of
+Loans",
+"Conventional 
+Loans: Total 
+Amount ($)",
+"Home Improvement:
+Validity Flag",
+"Home Improvement:
+Number or Loans",
+"Home Improvement:
+Total Amount ($)",
+"All Multi Family 
+Validity Flag",
+"All Multi Family:
+Number of Loans",
+"All Multi - Family:
+Total Amount ($)",
+"Non - Occupant
+Loans: Validity 1 
+Flag",
+"Non Occupant
+Loans: Number of
+Loans",
+"Non - Occupant 
+Loans: Total
+Amount ($)",
+"Record Quality
+Flag",
+"Filler")
+
+write.csv2(dat, "HMS.F1989.csv", row.names = FALSE)
